314f5d370f9c284710a2f8e0181ebd46006720e5
ttexan1 <ttexan.developper@gmail.com>
2020-11-26 12:58:45 +0900

    修正

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,6 @@ import express from 'express';
 import next from 'next';
 import { parse } from 'url';
 
-const DEFAULT_API_BASE_URL = 'https://console-api-staging.dooh-geniee.jp';
-
-// 空文字渡した時は自己責任ってことで
-const apiBaseUrl = process.env.API_BASE_URL ?? DEFAULT_API_BASE_URL;
 const isProduction = process.env.NODE_ENV === 'production';
 const app = next({ dev: isProduction });
 const handle = app.getRequestHandler();
@@ -14,13 +10,10 @@ const handle = app.getRequestHandler();
   await app.prepare();
   const server = express();
   server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', apiBaseUrl);
-    const schema = req.headers['x-forwarded-proto'];
-    console.log("AAA")
-    console.log(schema);
-    if (isProduction && schema !== 'https') {
-      return res.redirect(`https://${req.headers.host}${req.url}`);
-    }
+    // const schema = req.headers['x-forwarded-proto'];
+    // if (isProduction && schema !== 'https') {
+    //   return res.redirect(`https://${req.headers.host}${req.url}`);
+    // }
     next();
   });
   // handle nextjs routing
@@ -30,5 +23,5 @@ const handle = app.getRequestHandler();
   });
 
   await server.listen(8080);
-  console.log(`Ready on http://localhost:${8080}`); // eslint-disable-line no-console
+  console.log(`Ready on http://localhost:${8080}`);
 })();
